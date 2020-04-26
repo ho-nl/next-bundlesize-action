7a78fad0ee22b66082ef2763f7f166703d715a2b
Paul Hachmang <paul@reachdigital.nl>
2020-04-26 14:09:29 +0200

    chore: upgrade to checkout@v2

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
-import * as core from '@actions/core'
+import { debug, setOutput, setFailed, getInput } from '@actions/core'
 import { wait } from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const ms: string = getInput('milliseconds')
+    debug(`Waiting ${ms} milliseconds ...`)
 
-    core.debug(new Date().toTimeString())
+    debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
+    setOutput('time', new Date().toTimeString())
   } catch (error) {
-    core.setFailed(error.message)
+    setFailed(error.message)
   }
 }
 
