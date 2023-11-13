697e8ab026293fc498ca284a68ce3a43b50abf76
Paul Hachmang <paul@reachdigital.nl>
2023-11-13 15:32:23 +0100

    chore: move to inline snapshot

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -5,7 +5,15 @@ test('creates proper diff', async () => {
   const diff = await runDiff({
     GITHUB_WORKSPACE: path.join(__dirname, 'fixture-diff'),
   })
-  expect(diff).toMatchSnapshot()
+  expect(diff).toMatchInlineSnapshot(`
+    "| Page                   | Size old | Size new | Size diff | First load old | First load new | First load diff |
+    | ---------------------- | -------- | -------- | --------- | -------------- | -------------- | --------------- |
+    | /account/contact       | 7,9kB    | 7,9kB    |           | 254kB          | 253,0kB        | -1kB            |
+    | /test/icons            | 6,8kB    |          |           | 209kB          |                | 🗑              |
+    | /test/slider           | 1,8kB    | 1,8kB    |           | 235kB          | 249,0kB        | +14kB🚨         |
+    | /account/addresses/add |          | 5,3kB    | 🆕        |                | 255,0kB        | 🆕              |
+    "
+  `)
   console.log(`\n${diff}`)
 })
 
@@ -13,6 +21,6 @@ test('creates no diff', async () => {
   const diff = await runDiff({
     GITHUB_WORKSPACE: path.join(__dirname, 'fixture-same'),
   })
-  expect(diff).toMatchSnapshot()
+  expect(diff).toMatchInlineSnapshot(`"No change in bundle size detected 🎉"`)
   console.log(`\n${diff}`)
 })
